de2c94fc752f92cfa12c105aa85983de3be3e072
User <kenobi88@gmail.com>
2026-07-09 14:38:56 +0300

    2-commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,51 +1,113 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  ToastAndroid,
+  View,
+} from "react-native";
 
-export default function HomeScreen() {
+import { useState } from "react";
+
+export default function App() {
+  type Task = {
+    id: string;
+    text: string;
+  };
+
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = () => {
+    if (task.trim() === "") {
+      Alert.alert("Помилка", "Будь ласка, введіть текст задачі.");
+      return;
+    }
+    const newTask: Task = {
+      id: Date.now().toString(),
+      text: task,
+    };
+
+    setTasks([...tasks, newTask]);
+
+    ToastAndroid.show("Задачу успішно додано!", ToastAndroid.SHORT);
+
+    setTask("");
+  };
+
+  const deleteTask = (id: string) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.trafficLight}>
-        <View style={[styles.light, { backgroundColor: "red" }]}>
-          <Text style={styles.lightText}>STOP</Text>
-        </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Мій список задач</Text>
 
-        <View style={[styles.light, { backgroundColor: "yellow" }]}>
-          <Text style={styles.lightText}>READY</Text>
-        </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Введіть нову задачу..."
+        value={task}
+        onChangeText={setTask}
+      />
+      <Button title="Додати" onPress={addTask} />
 
-        <View style={[styles.light, { backgroundColor: "green" }]}>
-          <Text style={styles.lightText}>GO</Text>
-        </View>
-      </View>
-    </View>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.taskContainer}>
+            <Text style={styles.taskText}>{item.text}</Text>
+
+            <Button
+              title="Видалити"
+              color="red"
+              onPress={() => deleteTask(item.id)}
+            />
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#ffffff",
+    padding: 20,
+    backgroundColor: "#fff",
   },
 
-  trafficLight: {
-    backgroundColor: "#444",
-    padding: 15,
-    borderRadius: 15,
-  },
-
-  light: {
-    width: 90,
-    height: 90,
-    borderRadius: 45,
-    justifyContent: "center",
-    alignItems: "center",
-    marginVertical: 8,
-  },
-
-  lightText: {
-    color: "white",
+  title: {
+    fontSize: 28,
     fontWeight: "bold",
-    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#999",
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+    marginBottom: 15,
+  },
+
+  taskContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+
+  taskText: {
+    flex: 1,
+    fontSize: 18,
+    marginRight: 10,
   },
 });
